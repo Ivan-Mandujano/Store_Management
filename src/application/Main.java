bc151bcb552f885aa8c1d746aad66658ccf463de
Ivan-Mandujano <ivanmandujano594@gmail.com>
2023-12-13 17:19:22 -0600

    Create the FXML of the loggin screeen
    Using scene Builder created the view of the login

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,31 @@
 package application;
-	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.image.*;
+
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,Color.BLACK);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Demo de pantalla");
-			Image icon = new Image("Images/logo.png");
-			primaryStage.getIcons().add(icon);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    private static Stage stg;
+    @Override
+    public void start(Stage stage) throws Exception{
+        stg = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 350, 450);
+        stage.setResizable(false);
+        stage.setTitle("StoreApp");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
