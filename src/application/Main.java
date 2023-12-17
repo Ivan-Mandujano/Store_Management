5ded42bd72d372ca7a9f918447e1c1a33968dd02
Ivan-Mandujano <ivanmandujano594@gmail.com>
2023-12-16 19:11:50 -0600

    reworked version of main, to allow to have a modular form of displaying the stages by simply calling the loadView in the new main file

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,21 +12,53 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.image.*;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
-    private static Stage stg;
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
-    public void start(Stage stage) throws Exception{
-        stg = stage;
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LoginScene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 350, 450);
-        stage.setResizable(false);
-		Image icon = new Image("Images/logo.png");
-		stage.getIcons().add(icon);
-        stage.setTitle("StoreApp");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Stage stage = primaryStage;
+
+        // Llamada inicial para cargar la vista de inicio
+        loadView(stage, "LoginScene.fxml", "StoreApp Login", 350, 450);
     }
+
+    // Función para cambiar de vista
+    public void loadView(Stage primaryStage, String fxmlFile, String title, double width, double height) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = fxmlLoader.load();
+
+            // Añadir funcionalidad de arrastrar la ventana
+            root.setOnMousePressed(event -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
+
+            root.setOnMouseDragged(event -> {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            });
+
+            // Configurar el escenario y la ventana
+            //primaryStage.initStyle(StageStyle.UNDECORATED);
+            Scene scene = new Scene(root, width, height);
+            primaryStage.setResizable(false);
+            Image icon = new Image("Images/logo.png");
+            primaryStage.getIcons().add(icon);
+            primaryStage.setTitle(title);
+            primaryStage.setScene(scene);
+
+            // Mostrar la nueva vista
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
