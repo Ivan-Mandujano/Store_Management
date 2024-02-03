bda78833d4b9a28b3001fc10434b0ebf96beef3d
Ivan-Mandujano <ivanmandujano594@gmail.com>
2024-02-02 18:32:15 -0600

    New feature, when creating a new product the prodImage is send to the server. Plus few verifications
    Now when adding correctly or incorrectly a new product a popAlert will apear and the fields are cleaned

--- src/application/NewProductController.java
+++ src/application/NewProductController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
+import javafx.scene.control.Alert;
 
 import java.io.File;
 //URL
@@ -22,6 +23,18 @@ import java.nio.charset.StandardCharsets;
 //Json
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+//Apache HTTP
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
 public class NewProductController {
 	//BUTTONS
     @FXML
@@ -54,6 +67,9 @@ public class NewProductController {
     @FXML
     private Label EmptyQuantity;
     
+    //Path a la imagen del producto
+    private static String imagePath;
+
     @FXML
     private void handlePickImage() {
         // Configurar el File Chooser
@@ -70,6 +86,9 @@ public class NewProductController {
         if (selectedFile != null) {
             Image selectedImage = new Image(selectedFile.toURI().toString());
             imageView.setImage(selectedImage);
+
+            // Almacenar la ruta de la imagen
+            imagePath = selectedFile.getAbsolutePath();
         }
     }
     @FXML
@@ -78,6 +97,69 @@ public class NewProductController {
     	if(check) {
     		SendtoDatabase();
     	}
+    }
+    private void mostrarPopup(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("New product");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    private void borrarCampos() {
+        borrarTextField(name, category, price, quantity);
+        borrarTextArea(description);
+        borrarLabels(EmptyName, EmptyCategory, EmptyPrice, EmptyQuantity);
+        borrarImagen();
+    }
+
+    private void borrarTextField(TextField... campos) {
+        for (TextField campo : campos) {
+            campo.clear();
+        }
+    }
+
+    private void borrarTextArea(TextArea... areas) {
+        for (TextArea area : areas) {
+            area.clear();
+        }
+    }
+
+    private void borrarLabels(Label... labels) {
+        for (Label label : labels) {
+            label.setText("");
+        }
+    }
+    private void borrarImagen() {
+        imageView.setImage(null);
+    }
+    private static void uploadImage(String prod_id) throws IOException, ParseException {
+        File imageFile = new File(imagePath);
+
+        if (!imageFile.exists()) {
+            System.out.println("Image file not found.");
+            return;
+        }
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost("http://127.0.0.1:5000/prod_images/new");
+
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.addBinaryBody("image_data", imageFile);
+            builder.addTextBody("prod_id", prod_id);  // Replace with the actual prod_id value
+
+            HttpEntity multipartEntity = builder.build();
+            httpPost.setEntity(multipartEntity);
+
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+                ClassicHttpResponse classicResponse = response;
+                int statusCode = classicResponse.getCode();
+                System.out.println("Response Code: " + statusCode);
+
+                HttpEntity responseEntity = response.getEntity();
+                String responseBody = EntityUtils.toString(responseEntity);
+                System.out.println("Response: " + responseBody);
+            }
+        }
     }
     private void SendtoDatabase() {
         try {
@@ -110,8 +192,46 @@ public class NewProductController {
             // Obtiene la respuesta del servidor
             int responseCode = connection.getResponseCode();
             System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Lee la respuesta del servidor utilizando Jackson
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    StringBuilder response = new StringBuilder();
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+
+                    // Parsea la respuesta JSON con Jackson
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    JsonNode jsonResponse = objectMapper.readTree(response.toString());
+
+                    String status = jsonResponse.get("status").asText();
+                    String message = jsonResponse.get("message").asText();
+                    String mensaje;
+                    if ("success".equals(status)) {
+                        // Extrae el prod_id del response
+                        String prodId = jsonResponse.get("prod_id").asText();
+                        System.out.println("Prod_ID: " + prodId);
+                     // Llama al método para subir la imagen
+                        uploadImage(prodId);
+                        mensaje = "The new product was added successfully.";
+                        borrarCampos();
+                        mostrarPopup(mensaje);
+                    } else {
+                        System.out.println("Error: " + message);
+                        mensaje = "An error has ocurred during the process.";
+                        mostrarPopup(mensaje);
+                    }
+                }
+            } else {
+                System.out.println("Error in HTTP request. Response Code: " + responseCode);
+            }
+
             // Cierra la conexión
             connection.disconnect();
+
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -147,6 +267,8 @@ public class NewProductController {
     	}
     	if(quantity.getText().isEmpty()) {
     		EmptyQuantity.setText("Please enter the description");
+    		NoErrors=false;
+    	}if(imagePath.isEmpty()) {
     		NoErrors=false;
     	}else {
     		EmptyQuantity.setText("");
